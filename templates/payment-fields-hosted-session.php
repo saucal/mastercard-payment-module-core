4c01893e77574d652cf04c5b057b13eb692b5e55
Alejandro Iglesias <alexiglesias3110@gmail.com>
2025-01-30 12:09:14 +0100

    fix styles on generic themes

--- templates/payment-fields-hosted-session.php
+++ templates/payment-fields-hosted-session.php
@@ -19,21 +19,23 @@ if ( empty( $gateway ) || empty( $session_id ) || empty( $session_attempt ) ) {
 
 ?>
 <fieldset id="wc-<?php echo esc_attr( $gateway->id ); ?>-cc-form" class="wc-credit-card-form wc-payment-form <?php echo esc_attr( $gateway->id ); ?>-payment-form mpgs-payment-form" data-field-type="card">
-	<div class="form-row form-row-wide">
-		<label for="<?php echo esc_attr( $gateway->id ); ?>-card-number"><?php esc_html_e( 'Card number', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
-		<input id="<?php echo esc_attr( $gateway->id ); ?>-card-number-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-number" inputmode="numeric" autocomplete="cc-number" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" placeholder="&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;" />
-	</div>
-	<div class="form-row form-row-first">
-		<label for="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-month"><?php esc_html_e( 'Expiry (MM)', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
-		<input id="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-month-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-expiry" inputmode="numeric" autocomplete="cc-exp" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" placeholder="<?php esc_attr_e( 'MM', $gateway->mpgs_plugin()->text_domain() ); ?>" />
-	</div>
-	<div class="form-row form-row-last">
-		<label for="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-year"><?php esc_html_e( 'Expiry (YY)', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
-		<input id="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-year-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-expiry" inputmode="numeric" autocomplete="cc-exp" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" placeholder="<?php esc_attr_e( 'YY', $gateway->mpgs_plugin()->text_domain() ); ?>" />
-	</div>
-	<div class="form-row form-row-wide">
-		<label for="<?php echo esc_attr( $gateway->id ); ?>-card-cvc"><?php esc_html_e( 'Card code', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
-		<input id="<?php echo esc_attr( $gateway->id ); ?>-card-cvc-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-cvc" inputmode="numeric" autocomplete="off" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" maxlength="4" placeholder="<?php esc_attr_e( 'CVC', $gateway->mpgs_plugin()->text_domain() ); ?>" />
+	<div class="<?php echo esc_attr( $gateway->id ); ?>-payment-form-elements mpgs-payment-form-elements">
+		<div class="form-row form-row-wide">
+			<label for="<?php echo esc_attr( $gateway->id ); ?>-card-number"><?php esc_html_e( 'Card number', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
+			<input id="<?php echo esc_attr( $gateway->id ); ?>-card-number-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-number" inputmode="numeric" autocomplete="cc-number" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" placeholder="&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;" />
+		</div>
+		<div class="form-row form-row-first">
+			<label for="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-month"><?php esc_html_e( 'Expiry (MM)', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
+			<input id="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-month-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-expiry" inputmode="numeric" autocomplete="cc-exp" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" placeholder="<?php esc_attr_e( 'MM', $gateway->mpgs_plugin()->text_domain() ); ?>" />
+		</div>
+		<div class="form-row form-row-last">
+			<label for="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-year"><?php esc_html_e( 'Expiry (YY)', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
+			<input id="<?php echo esc_attr( $gateway->id ); ?>-card-expiry-year-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-expiry" inputmode="numeric" autocomplete="cc-exp" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" placeholder="<?php esc_attr_e( 'YY', $gateway->mpgs_plugin()->text_domain() ); ?>" />
+		</div>
+		<div class="form-row form-row-wide">
+			<label for="<?php echo esc_attr( $gateway->id ); ?>-card-cvc"><?php esc_html_e( 'Card code', $gateway->mpgs_plugin()->text_domain() ); ?><span class="required">*</span></label>
+			<input id="<?php echo esc_attr( $gateway->id ); ?>-card-cvc-<?php echo esc_attr( $session_id ); ?>" readonly="readonly" class="input-text wc-credit-card-form-card-cvc" inputmode="numeric" autocomplete="off" autocorrect="no" autocapitalize="no" spellcheck="no" type="tel" maxlength="4" placeholder="<?php esc_attr_e( 'CVC', $gateway->mpgs_plugin()->text_domain() ); ?>" />
+		</div>
 	</div>
 	<div class="clear"></div>
 	<input type="hidden" id="<?php echo esc_attr( $gateway->id ); ?>_session_id" name="<?php echo esc_attr( $gateway->id ); ?>_session_id" value="<?php echo esc_attr( $session_id ); ?>" />
